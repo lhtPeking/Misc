e80b25c29902b250f837be0386019e3359285ce2
Haotian Li <haotian.l@outlook.com>
2025-07-31 13:03:36 -0400

    image

---
+++ imaging_process/imaging_methods.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+'''
+    An illustration of data structure:
+    dff.shape = [neuron, slow_frame]
+    swims1.shape = [fast_frame]
+    swims2.shape = [fast_frame]
+    gain.shape = [fast_frame]
+    grating_speed.shape = [fast_frame]
+    frames.shape = [fast_frame]
+'''
+
+class Imaging:
+    def __init__(self, dff_path, frames_path):
+        self.dff_path = dff_path
+        self.frames_path = frames_path
+        
+        
+    
+class Behavior:
+    def __init__(self, swims1_path, swims2_path, gain_path, grating_speed_path):
+        self.swims1_path = swims1_path
+        self.swims2_path = swims2_path
+        self.gain_path = gain_path
+        self.grating_speed_path = grating_speed_path
+        
+        self.fast_frame_num = len(np.load(self.gain_path))
+        
+    def ephy_visualization(self):
+        pass
+    
+    
+
+class Experiment:
+    def __init__(self, imaging, behavior):
+        self.imaging = imaging
+        self.behavior = behavior
