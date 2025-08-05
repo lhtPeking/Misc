8cc8b1f843d4e8c1bba72ff64ce479454c6a55ee
Haotian Li <haotian.l@outlook.com>
2025-08-05 17:35:18 -0400

    a

--- imaging_process/imaging_methods.py
+++ imaging_process/imaging_methods.py
@@ -14,8 +14,12 @@ class Imaging:
     def __init__(self, dff_path, frames_path):
         self.dff_path = dff_path
         self.frames_path = frames_path
-        
-        
+    
+    def rastermap_visualization(self):
+        '''
+        A visualization method utilizing clustering -> cross-correlation sorting -> upsampling to preserve the structure between superneurons.
+        '''
+        pass
     
 class Behavior:
     def __init__(self, swims1_path, swims2_path, gain_path, grating_speed_path):
@@ -27,6 +31,9 @@ class Behavior:
         self.fast_frame_num = len(np.load(self.gain_path))
         
     def ephy_visualization(self):
+        '''
+        use both swims1 and swims2 data to visualize
+        '''
         pass
     
     
@@ -35,3 +42,6 @@ class Experiment:
     def __init__(self, imaging, behavior):
         self.imaging = imaging
         self.behavior = behavior
+        
+    def conjoint_analysis(self):
+        pass
